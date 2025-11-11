daecd474d85dba99f746fe98c3c06db0f5a988e4
katrinalyyy <k44trinalyyy@mail.ru>
2025-11-11 06:34:02 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ChronicleSearch_frontend/sw.js', { scope: '/ChronicleSearch_frontend/' })})}
